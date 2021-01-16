b941e5c7d59bc9d2f5684531df38996bdb105a01
Tekena Solomon <tekena157@gmail.com>
2021-01-17 00:44:15 +0100

    Add ImageLinkForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-    <FaceRecogniton /> */}
+      <ImageLinkForm />
+      {/* <FaceRecogniton /> */}
     </div>
   );
 }
---
+++ src/components/ImageLinkForm/ImageLinkForm.tsx
@@ -0,0 +1,28 @@
+import "./ImageLinkForm.css";
+
+const Imagelinkform = () => {
+  return (
+    <div>
+      <p className="f3 center">
+        {"This magic brain will detect faces in your pictures. Give it a try"}
+      </p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            className="f4 pa2 w-70 center"
+            type="tex"
+            // onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            // onClick={onButtonSubmit}
+          >
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Imagelinkform;
