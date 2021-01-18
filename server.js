0e25faffaa0a2a896c7217899b6c20951bc56808
Tekena Solomon <tekena157@gmail.com>
2021-01-18 21:18:24 +0100

    Add signin route

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -7,11 +12,37 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.send("SmartBrain API");
+  res.send("Welcome to SmartBrain API!");
 });
 
-app.post("/signin", (req, res) => {
-  res.send({ msg: "working signing" });
+app.post("/signin", async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+    if (!email || !password || !name) {
+      return res
+        .status(400)
+        .json({ msg: "`email`, `password` and `name` is required" });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: bcrypt.hashSync(password, 10),
+      },
+    });
+
+    const token = jwt.sign(
+      { id: user.id, name: user.name },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2 days",
+      }
+    );
+    res.send({ user, token });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.post("/register", (req, res) => {
